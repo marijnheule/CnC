fa1c463dccd5b54704373e60572fcce7ba3e52a2
marijn <marijn@heule.nl>
2018-05-29 14:30:02 +0200

    build

---
+++ build.sh
@@ -0,0 +1,2 @@
+cd march_cu; make $1; cd ..;
+cd iglucose/core; make $1; cd ../simp; make $1; cd ../..;
--- iglucose/simp/depend.mk
+++ iglucose/simp/depend.mk
@@ -1,68 +1,68 @@
-/Users/marijn/iglucose/simp/Main.o /Users/marijn/iglucose/simp/Main.or /Users/marijn/iglucose/simp/Main.od /Users/marijn/iglucose/simp/Main.op: /Users/marijn/iglucose/simp/Main.cc \
-  /Users/marijn/iglucose/simp/../utils/System.h \
-  /Users/marijn/iglucose/simp/../mtl/IntTypes.h \
-  /Users/marijn/iglucose/simp/../utils/ParseUtils.h \
-  /Users/marijn/iglucose/simp/../utils/Options.h \
-  /Users/marijn/iglucose/simp/../mtl/Vec.h \
-  /Users/marijn/iglucose/simp/../mtl/XAlloc.h \
-  /Users/marijn/iglucose/simp/../core/Dimacs.h \
-  /Users/marijn/iglucose/simp/../core/SolverTypes.h \
-  /Users/marijn/iglucose/simp/../mtl/Alg.h \
-  /Users/marijn/iglucose/simp/../mtl/Map.h \
-  /Users/marijn/iglucose/simp/../mtl/Alloc.h \
-  /Users/marijn/iglucose/simp/../simp/SimpSolver.h \
-  /Users/marijn/iglucose/simp/../mtl/Queue.h \
-  /Users/marijn/iglucose/simp/../core/Solver.h \
-  /Users/marijn/iglucose/simp/../mtl/Heap.h \
-  /Users/marijn/iglucose/simp/../core/BoundedQueue.h \
-  /Users/marijn/iglucose/simp/../core/Constants.h
-/Users/marijn/iglucose/simp/SimpSolver.o /Users/marijn/iglucose/simp/SimpSolver.or /Users/marijn/iglucose/simp/SimpSolver.od /Users/marijn/iglucose/simp/SimpSolver.op: /Users/marijn/iglucose/simp/SimpSolver.cc \
-  /Users/marijn/iglucose/simp/../mtl/Sort.h \
-  /Users/marijn/iglucose/simp/../mtl/Vec.h \
-  /Users/marijn/iglucose/simp/../mtl/IntTypes.h \
-  /Users/marijn/iglucose/simp/../mtl/XAlloc.h \
-  /Users/marijn/iglucose/simp/../simp/SimpSolver.h \
-  /Users/marijn/iglucose/simp/../mtl/Queue.h \
-  /Users/marijn/iglucose/simp/../core/Solver.h \
-  /Users/marijn/iglucose/simp/../mtl/Heap.h \
-  /Users/marijn/iglucose/simp/../mtl/Alg.h \
-  /Users/marijn/iglucose/simp/../utils/Options.h \
-  /Users/marijn/iglucose/simp/../utils/ParseUtils.h \
-  /Users/marijn/iglucose/simp/../core/SolverTypes.h \
-  /Users/marijn/iglucose/simp/../mtl/Map.h \
-  /Users/marijn/iglucose/simp/../mtl/Alloc.h \
-  /Users/marijn/iglucose/simp/../core/BoundedQueue.h \
-  /Users/marijn/iglucose/simp/../core/Constants.h \
-  /Users/marijn/iglucose/simp/../utils/System.h
-/Users/marijn/iglucose/core/Main.o /Users/marijn/iglucose/core/Main.or /Users/marijn/iglucose/core/Main.od /Users/marijn/iglucose/core/Main.op: /Users/marijn/iglucose/core/Main.cc \
-  /Users/marijn/iglucose/core/../utils/System.h \
-  /Users/marijn/iglucose/core/../mtl/IntTypes.h \
-  /Users/marijn/iglucose/core/../utils/ParseUtils.h \
-  /Users/marijn/iglucose/core/../utils/Options.h \
-  /Users/marijn/iglucose/core/../mtl/Vec.h \
-  /Users/marijn/iglucose/core/../mtl/XAlloc.h \
-  /Users/marijn/iglucose/core/../core/Dimacs.h \
-  /Users/marijn/iglucose/core/../core/SolverTypes.h \
-  /Users/marijn/iglucose/core/../mtl/Alg.h \
-  /Users/marijn/iglucose/core/../mtl/Map.h \
-  /Users/marijn/iglucose/core/../mtl/Alloc.h \
-  /Users/marijn/iglucose/core/../core/Solver.h \
-  /Users/marijn/iglucose/core/../mtl/Heap.h \
-  /Users/marijn/iglucose/core/../core/BoundedQueue.h \
-  /Users/marijn/iglucose/core/../core/Constants.h
-/Users/marijn/iglucose/core/Solver.o /Users/marijn/iglucose/core/Solver.or /Users/marijn/iglucose/core/Solver.od /Users/marijn/iglucose/core/Solver.op: /Users/marijn/iglucose/core/Solver.cc \
-  /Users/marijn/iglucose/core/../mtl/Sort.h \
-  /Users/marijn/iglucose/core/../mtl/Vec.h \
-  /Users/marijn/iglucose/core/../mtl/IntTypes.h \
-  /Users/marijn/iglucose/core/../mtl/XAlloc.h \
-  /Users/marijn/iglucose/core/../core/Solver.h \
-  /Users/marijn/iglucose/core/../mtl/Heap.h \
-  /Users/marijn/iglucose/core/../mtl/Alg.h \
-  /Users/marijn/iglucose/core/../utils/Options.h \
-  /Users/marijn/iglucose/core/../utils/ParseUtils.h \
-  /Users/marijn/iglucose/core/../core/SolverTypes.h \
-  /Users/marijn/iglucose/core/../mtl/Map.h \
-  /Users/marijn/iglucose/core/../mtl/Alloc.h \
-  /Users/marijn/iglucose/core/../core/BoundedQueue.h \
-  /Users/marijn/iglucose/core/../core/Constants.h \
-  /Users/marijn/iglucose/core/../utils/System.h
+/Users/marijn/CnC/iglucose/simp/Main.o /Users/marijn/CnC/iglucose/simp/Main.or /Users/marijn/CnC/iglucose/simp/Main.od /Users/marijn/CnC/iglucose/simp/Main.op: /Users/marijn/CnC/iglucose/simp/Main.cc \
+  /Users/marijn/CnC/iglucose/simp/../utils/System.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/IntTypes.h \
+  /Users/marijn/CnC/iglucose/simp/../utils/ParseUtils.h \
+  /Users/marijn/CnC/iglucose/simp/../utils/Options.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/Vec.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/XAlloc.h \
+  /Users/marijn/CnC/iglucose/simp/../core/Dimacs.h \
+  /Users/marijn/CnC/iglucose/simp/../core/SolverTypes.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/Alg.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/Map.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/Alloc.h \
+  /Users/marijn/CnC/iglucose/simp/../simp/SimpSolver.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/Queue.h \
+  /Users/marijn/CnC/iglucose/simp/../core/Solver.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/Heap.h \
+  /Users/marijn/CnC/iglucose/simp/../core/BoundedQueue.h \
+  /Users/marijn/CnC/iglucose/simp/../core/Constants.h
+/Users/marijn/CnC/iglucose/simp/SimpSolver.o /Users/marijn/CnC/iglucose/simp/SimpSolver.or /Users/marijn/CnC/iglucose/simp/SimpSolver.od /Users/marijn/CnC/iglucose/simp/SimpSolver.op: /Users/marijn/CnC/iglucose/simp/SimpSolver.cc \
+  /Users/marijn/CnC/iglucose/simp/../mtl/Sort.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/Vec.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/IntTypes.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/XAlloc.h \
+  /Users/marijn/CnC/iglucose/simp/../simp/SimpSolver.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/Queue.h \
+  /Users/marijn/CnC/iglucose/simp/../core/Solver.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/Heap.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/Alg.h \
+  /Users/marijn/CnC/iglucose/simp/../utils/Options.h \
+  /Users/marijn/CnC/iglucose/simp/../utils/ParseUtils.h \
+  /Users/marijn/CnC/iglucose/simp/../core/SolverTypes.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/Map.h \
+  /Users/marijn/CnC/iglucose/simp/../mtl/Alloc.h \
+  /Users/marijn/CnC/iglucose/simp/../core/BoundedQueue.h \
+  /Users/marijn/CnC/iglucose/simp/../core/Constants.h \
+  /Users/marijn/CnC/iglucose/simp/../utils/System.h
+/Users/marijn/CnC/iglucose/core/Main.o /Users/marijn/CnC/iglucose/core/Main.or /Users/marijn/CnC/iglucose/core/Main.od /Users/marijn/CnC/iglucose/core/Main.op: /Users/marijn/CnC/iglucose/core/Main.cc \
+  /Users/marijn/CnC/iglucose/core/../utils/System.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/IntTypes.h \
+  /Users/marijn/CnC/iglucose/core/../utils/ParseUtils.h \
+  /Users/marijn/CnC/iglucose/core/../utils/Options.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/Vec.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/XAlloc.h \
+  /Users/marijn/CnC/iglucose/core/../core/Dimacs.h \
+  /Users/marijn/CnC/iglucose/core/../core/SolverTypes.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/Alg.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/Map.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/Alloc.h \
+  /Users/marijn/CnC/iglucose/core/../core/Solver.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/Heap.h \
+  /Users/marijn/CnC/iglucose/core/../core/BoundedQueue.h \
+  /Users/marijn/CnC/iglucose/core/../core/Constants.h
+/Users/marijn/CnC/iglucose/core/Solver.o /Users/marijn/CnC/iglucose/core/Solver.or /Users/marijn/CnC/iglucose/core/Solver.od /Users/marijn/CnC/iglucose/core/Solver.op: /Users/marijn/CnC/iglucose/core/Solver.cc \
+  /Users/marijn/CnC/iglucose/core/../mtl/Sort.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/Vec.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/IntTypes.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/XAlloc.h \
+  /Users/marijn/CnC/iglucose/core/../core/Solver.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/Heap.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/Alg.h \
+  /Users/marijn/CnC/iglucose/core/../utils/Options.h \
+  /Users/marijn/CnC/iglucose/core/../utils/ParseUtils.h \
+  /Users/marijn/CnC/iglucose/core/../core/SolverTypes.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/Map.h \
+  /Users/marijn/CnC/iglucose/core/../mtl/Alloc.h \
+  /Users/marijn/CnC/iglucose/core/../core/BoundedQueue.h \
+  /Users/marijn/CnC/iglucose/core/../core/Constants.h \
+  /Users/marijn/CnC/iglucose/core/../utils/System.h
