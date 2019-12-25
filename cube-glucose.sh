1295837ffd6d13fe092a86e9ab52532dfc9620fa
Marienus Heule <mheule@SCS003B8.local>
2019-12-25 13:07:01 +0100

    histogram

--- cube-glucose.sh
+++ cube-glucose.sh
@@ -1,12 +1,13 @@
 CNF=$1
-DIR=/tmp
+DIR=~/CnC/
+OUT=/tmp
 #~/Folkman/vdW/march_cu/march_cu $CNF -o $DIR/cubes$$ $2 $3 $4 $5 $6 $7 $8 $9
-./march_cu/march_cu $CNF -o $DIR/cubes$$ $2 $3 $4 $5 $6 $7 $8 $9
+$DIR/march_cu/march_cu $CNF -o $OUT/cubes$$ $2 $3 $4 $5 $6 $7 $8 $9
 echo "p inccnf" > $DIR/formula$$.icnf
-cat $CNF | grep -v c >> $DIR/formula$$.icnf
-cat $DIR/cubes$$ >> $DIR/formula$$.icnf
-rm  $DIR/cubes$$
-./iglucose/core/iglucose $DIR/formula$$.icnf -verb=0
+cat $CNF | grep -v c >> $OUT/formula$$.icnf
+cat $OUT/cubes$$ >> $OUT/formula$$.icnf
+rm  $OUT/cubes$$
+$DIR/iglucose/core/iglucose $OUT/formula$$.icnf -verb=0
 #./iglucose/core/iglucose $DIR/formula$$.icnf -verb=0 -certified -certified-output=proof | grep -v bound
-rm $DIR/formula$$.icnf
+rm $OUT/formula$$.icnf
 
