ee8f8aab3729b46bc92dc16a169187fe0b5d8a9a
Heule <mheule@a483e71b7bd5.ant.amazon.com>
2020-02-07 15:18:56 -0500

    par-cube-glucose on OSX

--- par-cube-glucose.sh
+++ par-cube-glucose.sh
@@ -6,22 +6,25 @@ OUT=/tmp
 rm $OUT/output*.txt
 $DIR/march_cu/march_cu $CNF -o $OUT/cubes$$ $2 $3 $4 $5 $6 $7 $8 $9
 
-while [ 1 ]
+FLAG=1
+while [[ $FLAG == "1" ]]
 do
   cat $OUT/output*.txt | grep "SAT" | awk '{print $1}' | sort | uniq -c | tr "\n" "\t";
+   
   SAT=`cat $OUT/output*.txt | grep "^SAT" | awk '{print $1}' | uniq`
   if [[ $SAT == "SAT" ]]
   then
     echo "DONE"
     pkill -TERM -P $$
+    FLAG=0
   fi
+
   UNSAT=`cat $OUT/output*.txt | grep "^UNSAT" | wc |awk '{print $1}'`
-  if [[ $UNSAT == $PAR ]]
-  then
-    echo "DONE"
-    pkill -TERM -P $$
-  fi
-  sleep 1;
+  echo $UNSAT $PAR
+  if [[ $UNSAT == $PAR ]]; then echo "c ALL JOBS UNSAT"; FLAG=0; break; fi
+  ALIVE=`ps $$ | wc | awk '{print $1}'`
+  if [[ $ALIVE == "1" ]]; then echo "c PARENT TERMINATED"; FLAG=0; break; fi 
+  if [[ $FLAG  == "1" ]]; then sleep 1; fi
 done &
 
 for (( CORE=0; CORE<$PAR; CORE++ ))
